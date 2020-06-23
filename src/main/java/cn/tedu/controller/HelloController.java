a98982929e6464cf91a95a8df725dc497412faff
余浩 <980187223@qq.com>
2020-06-23 11:00:49 +0800

    自定义日期格式转换器

--- src/main/java/cn/tedu/controller/HelloController.java
+++ src/main/java/cn/tedu/controller/HelloController.java
@@ -8,9 +8,17 @@ package cn.tedu.controller;
      *
      * 当前类为普通类
      * */
+import cn.tedu.pojo.User;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.ServletRequestDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
-    /*
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/*
       *  @@Controller作用1：标识当前类属于Controller层
        * 如果Controller类上没有访问路径，当前方法上的访问路径在所有Controller类中不可以重复
     * */
@@ -54,6 +62,40 @@ public class HelloController {
             System.out.println("psw="+psw);
             System.out.println("addr="+addr);
             System.out.println("age="+age);
+            return "test";
+        }
+        /*
+        * 3.包装类型的参数绑定
+        * http://localhost:8080/springmvc
+         * /testParam03?user=刘德华&age=18&addr=中国香港
+         * 如果请求参数过多，或者希望将请求参数的值封装到一个对象，
+         * 可以直接在方法上声明一个包装类型的对象，使用包装对象接收请求中的参数值
+         * 要求包装对象中存在请求参数名对应的setter方法
+         * */
+        @RequestMapping("/testParam03")
+        public String testParam03(User user){
+            System.out.println("user.name="+user.getName());
+            System.out.println("user.age="+user.getAge());
+            System.out.println("user.addr="+user.getAddr());
+            return "test";
+        }
+    /* 自定义日期格式转换器
+     * 将springmvc默认以斜杠(/)分隔日期改为以横杠分隔(-)
+     */
+    @InitBinder
+    public void InitBinder (ServletRequestDataBinder binder){
+        binder.registerCustomEditor(java.util.Date.class,
+                new CustomDateEditor(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"), true)
+        );
+    }
+        /*
+        * 4.日期类的参数绑定
+        * /testParam04?date=2020/06/23 10:51:30
+        * /testParam04?date=2020-06-23 10:51:30
+        * */
+        @RequestMapping("/testParam04")
+        public String testParam04(Date date){
+            System.out.println("date="+date);
             return "test";
         }
 }
---
+++ src/main/java/cn/tedu/pojo/User.java
@@ -0,0 +1,43 @@
+package cn.tedu.pojo;
+
+public class User {
+    private String name;
+    private Integer age;
+    private String addr;
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddr() {
+        return addr;
+    }
+
+    public void setAddr(String addr) {
+        this.addr = addr;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", addr='" + addr + '\'' +
+                '}';
+    }
+}
